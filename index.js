8ce7c1e1f27123317844e93109141ed713f7d634
David Richardson <david.richardson@enquora.com>
2024-06-11 18:23:19 -0600

    Version 1.0.1

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     args : require('./args.js'),
     filelist : require('./filelist.js'),
     term : require('./term.js'),
-    util : require('./util.js')
+    util : require('./util.js'),
+    file : require('./file.js')
  }
