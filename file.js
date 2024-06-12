be097d03cf2757c96d3aa8403b5cef84d2761a7e
David Richardson <david.richardson@enquora.com>
2024-06-11 18:25:27 -0600

    Version 1.0.3

--- file.js
+++ file.js
@@ -13,6 +13,17 @@ require("./file-bootstrap");
 
 /* streams */
 
+/*** RegExp.escape
+    accepts a string; returns the string with regex metacharacters escaped.
+    the returned string can safely be used within a regex to match a literal
+    string. escaped characters are [, ], {, }, (, ), -, *, +, ?, ., \, ^, $,
+    |, #, [comma], and whitespace.
+*/
+RegExp.escape = function (str) {
+    return str.replace(/[-[\]{}()*+?.\\^$|,#\s]/g, "\\$&");
+};
+
+
 exports.open = function (path, mode, options) {
 
     // it's possible to confuse options and mode,
